4d2576f965947a68f5eba63911b00226928a207d
patilvicky104 <patilvicky104@gmail.com>
2021-03-30 20:04:45 +0530

    last

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +141,4 @@ LOGIN_REDIRECT_URL = "test"
 LOGOUT_REDIRECT_URL = "thanks"
 
 
+django_heroku.settings(locals())
