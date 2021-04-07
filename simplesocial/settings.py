3e440e126259f08888dec66dc042a4a9101e4434
patilvicky104 <50688824+patilvicky104@users.noreply.github.com>
2021-04-07 22:30:32 +0530

    Update settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -26,9 +26,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','vikrant-patil.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','vikrant-patil.herokuapp.com','localhost']
 
 
 # Application definition
