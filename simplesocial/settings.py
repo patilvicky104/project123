5169f4ee3d195c7ee2d3f8e723c8f5103860312c
patilvicky104 <50688824+patilvicky104@users.noreply.github.com>
2021-04-08 00:06:26 +0530

    Update settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','vikrant-patil.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','vikrant-patil.herokuapp.com','localhost','www.vikrantpatil.live','vikrantpatil.live']
 
 
 # Application definition
